4a33296df3e3d3bc7a97824aa2bb23af2ba205e3
zizo2k21 <aziz@LAPTOP-QGRKF93V>
2023-11-08 13:45:03 +0100

    feat:error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import intToRoman from './functions/intToRoman'; 
+import intToRoman from './functions/intToRoman';
 
 function App() {
-  const [number, setNumber] = useState<number | null>(null); 
-  const [romanNumeral, setRomanNumeral] = useState<string>(''); 
+  const [number, setNumber] = useState<number | null>(null);
+  const [romanNumeral, setRomanNumeral] = useState<string>('');
+  const [runtimeError, setRuntimeError] = useState<string>(''); // Annotation de type ajoutée pour 'runtimeError'
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const inputNumber = parseInt(event.target.value, 10);
     setNumber(inputNumber);
-    if (!isNaN(inputNumber)) {
-      setRomanNumeral(intToRoman(inputNumber));
-    } else {
-      setRomanNumeral(''); 
+
+    try {
+      if (!isNaN(inputNumber)) {
+        setRomanNumeral(intToRoman(inputNumber));
+        setRuntimeError(''); // Réinitialise les erreurs de runtime en cas de succès
+      } else {
+        setRomanNumeral('');
+        setRuntimeError(''); // Réinitialise les erreurs de runtime en cas de saisie incorrecte
+      }
+    } catch (error) {
+      if (typeof error === 'string') { // Vérifie que 'error' est bien une chaîne de caractères
+        setRuntimeError(error);
+      } else {
+        setRuntimeError('Number must be positive or integer or below 3999'); // Gestion par défaut si 'error' n'est pas une chaîne
+      }
     }
   };
 
@@ -25,7 +36,8 @@ function App() {
           placeholder="Entrez un nombre"
           onChange={handleInputChange}
         />
-        <p>Résultat en chiffres romains : {romanNumeral}</p>
+        {!runtimeError &&<p>Résultat en chiffres romains : {romanNumeral}</p>}
+        {runtimeError && <div className="error-message">{runtimeError}</div>}
       </header>
     </div>
   );
